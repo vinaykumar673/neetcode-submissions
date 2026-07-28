160adddbd5bdafb5b2a3c859e8294c7c7773b01a
Vinay Kumar <vinayks673@gmail.com>
2026-07-28 22:58:42 +0530

    Add: move-zeroes - submission-0

---
+++ Data Structures & Algorithms/move-zeroes/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+        int slow = 0;
+
+        for(int fast = 0; fast < nums.size(); fast++) {
+            if(nums[fast] != 0) {
+                swap(nums[slow], nums[fast]);
+                slow++;
+            }
+        }
+
+    }
+};
